ccb93fdf51ebcc978a4d0c8197090195b91966ae
jweezy <jwest1@luc.edu>
2019-02-28 11:14:03 -0600

    simple main menu from tutorial

--- pong/main.py
+++ pong/main.py
@@ -1,33 +1,72 @@
 import pygame
+import time
+import random
+import main_menu
 
+#most code is from here https://pythonprogramming.net/pygame-start-menu-tutorial/
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
-done = False
-is_blue = True
-x = 30
-y = 30
-clock = pygame.time.Clock()
 
-while not done:
+
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+
+    x_change = 0
+
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 4
+    thing_width = 100
+    thing_height = 100
+
+    thingCount = 1
+
+    dodged = 0
+
+    gameExit = False
+
+    while not gameExit:
+
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        done = True
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                        is_blue = not is_blue
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                main_menu.quit()
 
-        screen.fill((0,0,0))
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]: y -= 3
-        if pressed[pygame.K_DOWN]: y += 3
-        if pressed[pygame.K_LEFT]: x -= 3
-        if pressed[pygame.K_RIGHT]: x += 3
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
 
-        if is_blue:
-            color = (0, 128, 255)
-        else:
-            color = (255, 100, 0)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
 
-        pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
+        x += x_change
+        main_menu.gameDisplay.fill(white)
+
+        main_menu.things(thing_startx, thing_starty, thing_width, thing_height, block_color)
+
+
+
+        thing_starty += thing_speed
+        main_menu.things_dodged(dodged)
+
+
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0,display_width)
+            dodged += 1
+            thing_speed += 1
+            thing_width += (dodged * 1.2)
+
+        if y < thing_starty+thing_height:
+            print('y crossover')
+
+        pygame.display.update()
         clock.tick(60)
 
-        pygame.display.flip()
+main_menu.game_intro()
+game_loop()
+pygame.quit()
+quit()
---
+++ pong/main_menu.py
@@ -0,0 +1,70 @@
+import pygame
+import time
+import random
+
+#most code is from here https://pythonprogramming.net/pygame-start-menu-tutorial/
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+block_color = (53,115,255)
+
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('A bit Racey')
+clock = pygame.time.Clock()
+
+
+
+def things_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: "+str(count), True, black)
+    gameDisplay.blit(text,(0,0))
+
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop()
+
+
+
+def crash():
+    message_display('You Crashed')
+
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf',85)
+        TextSurf, TextRect = text_objects("Pong", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+        pygame.display.update()
+        clock.tick(15)
