3a3fd31d734decdc80d357f94e44a4a3d03b2717
jweezy <jwest1@luc.edu>
2019-02-28 11:06:05 -0600

    server ideas

--- pong/player.py
+++ pong/player.py
@@ -2,3 +2,16 @@ import pygame
 
 
 class Player:
+
+    def __init__(self, username, ip, side):
+        self.username = username
+        self.ip = ip
+        self.side = side
+        self.position = 0
+        self.score = 0
+
+    def move(self, val):
+        self.position += val
+
+    def update_score(self):
+        self.score+=1
---
+++ server/serverMain.py
@@ -0,0 +1,35 @@
+import socket
+import time
+import os
+import sys
+import json
+
+class match_maker:
+    def __init__(self):
+        #self.server_address = 'localhost'
+        self.connection = None
+        self.lobbies = []
+        self.player_queue = []
+        self.threads = []
+    def init_network(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.bind(('0.0.0.0',7999))
+
+    def listen(self):
+        message = ''
+        address = ''
+        try:
+            message, address = self.server_socket.recvfrom(1024)
+        except socket.timeout:
+            print("timeout")
+            return
+        self.parse_json(message)
+
+    def parse_json(self,packet):
+        try:
+            json_message = json.loads(packet)
+            if json_message["op"] == "searching":
+                player_queue.append((json_message["username"], json_message))
+
+        except NameError:
+            print('Incorrect Json format')
