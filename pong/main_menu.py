a5d749afc6da56862c5525781fd8f1a5b3c07ebe
jack <jbroncato@luc.edu>
2019-03-21 17:22:39 -0500

    Added menu with button that advances to game once clicked

--- pong/main_menu.py
+++ pong/main_menu.py
@@ -65,6 +65,49 @@ def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
+def button(msg,x,y,w,h,ic,ac):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+        if click[0] == 1:
+            return False
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("freesansbold.ttf",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+    return True
+
+def menu():
+
+    intro = True
+    green = (0,200,0)
+    bright_green = (0,255,0)
+
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf',115)
+        TextSurf, TextRect = text_objects("Click start", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        mouse = pygame.mouse.get_pos()
+
+        intro = button("Start",150,450,100,50,green,bright_green)
+
+        pygame.display.update()
+        clock.tick(15)
+        return (intro)
 
 def game_intro(sock):
     create_listen_thread(sock)
