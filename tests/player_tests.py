dfcd4ac16c7e1f1084d2cb5c2a8008f8e7dd74c0
jweezy <jwest1@luc.edu>
2019-04-14 15:58:09 -0500

    fixed tests

--- tests/player_tests.py
+++ tests/player_tests.py
@@ -11,5 +11,11 @@ class TestPlayerMethods(unittest.TestCase):
         self.assertTrue(self.player.side == 'L')
         self.assertTrue(self.player.position == 100)
 
+    def test_move(self):
+        holder = self.player.y
+        self.assertTrue(self.player.y == holder)
+        self.player.move(10)
+        self.assertTrue(self.player.y == holder+10)
+
 
 unittest.main()
