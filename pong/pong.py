13e25924064f38ebaeb903a2a529c8a05ebf2cbe
jweezy <jwest1@luc.edu>
2019-04-20 11:16:44 -0500

    fixing syntax problems with json translating for game server

--- pong/pong.py
+++ pong/pong.py
@@ -212,7 +212,7 @@ def first_phase():
         local_username = username
         print(username + "username")
         if username == json_message["username1"][0]:
-            game_server = json_message["username2"][1]
+            game_server = (json_message["username2"][1][0], json_message["username2"][1][1])
             print(str(json_message["username2"][1])+ " game server")
         else:
             game_server = json_message["username1"][1]
