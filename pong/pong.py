5b178bfd3375181b2358fc29e4c80627b6773251
jweezy <jwest1@luc.edu>
2019-04-20 10:47:31 -0500

    updated debugging

--- pong/pong.py
+++ pong/pong.py
@@ -213,10 +213,10 @@ def first_phase():
         print(username + "username")
         if username == json_message["username1"]:
             game_server = json_message["username2"][1]
-            print(json_message["username2"][1]+ " game server")
+            print(str(json_message["username2"][1])+ " game server")
         else:
             game_server = json_message["username1"][1]
-            print(json_message["username2"][1]+ " game server")
+            print(str(json_message["username1"][1])+ " game server")
         pong(json_message["username1"][0], json_message["username2"][0], message, game_server)
 
 if __name__ == '__main__':
