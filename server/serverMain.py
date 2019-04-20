20db5d1b2138d99d400c13195a0ec6c7ef3177bd
jweezy <jwest1@luc.edu>
2019-04-20 09:31:16 -0500

    fixed server error

--- server/serverMain.py
+++ server/serverMain.py
@@ -41,7 +41,7 @@ class match_maker:
             print(address)
             json_message = json.loads(packet)
             if json_message["op"] == "searching" and not self.player_in_queue(json_message['username']):
-                self.player_queue.append((json_message["username"], json_message))
+                self.player_queue.append((json_message["username"], json_message, (address[0], json_message["port"])))
                 if self.new_player(json_message["username"]):
                     self.write_player_to_memory(json_message["username"])
 
