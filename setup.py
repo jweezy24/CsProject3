51350f7099a31eb1a4f9e0a4ef338fe7ea2696f0
jweezy <jwest1@luc.edu>
2019-04-22 21:22:12 -0500

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,10 +1,40 @@
-import sys
-import os
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
 
-os.system('export PYTHONPATH="$PYTHONPATH:$HOME/CsProject3"')
+here = path.abspath(path.dirname(__file__))
 
-sys.path.append("./server")
-sys.path.append("./pong")
-sys.path.append("./tests")
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
 
-os.system("python3 ./server/serverMain.py")
+setup(
+    # This is the name of your project. The first time you publish this
+    # package, this name will be registered for you. It will determine how
+    # users can install this project, e.g.:
+    #
+    # $ pip install sampleproject
+    #
+    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
+    #
+    # There are some restrictions on what makes a valid project name
+    # specification here:
+    # https://packaging.python.org/specifications/core-metadata/#name
+    name='Pong',  # Required
+
+    # Versions should comply with PEP 440:
+    # https://www.python.org/dev/peps/pep-0440/
+    #
+    # For a discussion on single-sourcing the version across setup.py and the
+    # project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.8.0',  # Required
+
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(exclude=['contrib', 'docs', 'tests'])  # Required
+)
