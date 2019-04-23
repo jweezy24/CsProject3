f5823e3929e21f40eadef826035f1506fcdce0a0
jweezy <jwest1@luc.edu>
2019-04-22 21:14:15 -0500

    added more to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import sys
+import os
+
+os.system('export PYTHONPATH="$PYTHONPATH:$HOME/.python"')
 
 sys.path.append("./server")
 sys.path.append("./pong")
 sys.path.append("./tests")
+
+os.system("python3 ./server/serverMain.py")
